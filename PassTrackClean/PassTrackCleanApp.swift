31c7df71abd3945619ba5876f3e90aab97fa5f07
Tyler Roukey <troukey@icloud.com>
2026-01-08 21:42:05 -0500

    Initial Commit

---
+++ PassTrackClean/PassTrackCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassTrackCleanApp.swift
+//  PassTrackClean
+//
+//  Created by Tyler Roukey on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct PassTrackCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
